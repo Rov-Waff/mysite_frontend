2e5a0459bd0720d4218f9966081fc72e9adb7cbf
Rov-Waff <whanxiao885@gmail.com>
2025-10-01 19:26:15 +0800

    init repo

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div id="header">
+    <el-menu>
+      <span>Xiaole6324工具包</span>
+    </el-menu>
+  </div>
+</template>
+
+<style scoped></style>
