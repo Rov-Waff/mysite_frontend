0421aeaa197c84df850140418ac3790225b015be
Rov-Waff <whanxiao885@gmail.com>
2025-10-02 14:51:33 +0800

    妙哉！初具形态！

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div id="header">
-    <el-menu>
-      <span>Xiaole6324工具包</span>
-    </el-menu>
+  <el-menu mode="horizontal" router>
+    <el-menu-item index="/"
+      ><el-icon color="#ffffff"><House /></el-icon>云中乐土</el-menu-item
+    >
+    <el-sub-menu index="/tools">
+      <template #title>
+        <el-icon><Grid /></el-icon>小工具</template
+      >
+      <el-menu-item index="/tools/holiday">你的假期</el-menu-item>
+      <el-menu-item index="/tools/clock">倒计时</el-menu-item>
+      <el-menu-item index="/tools/random">随机数</el-menu-item>
+    </el-sub-menu>
+    <el-menu-item index="/articles">
+      <el-icon><Document /></el-icon>文章</el-menu-item
+    >
+    <el-menu-item index="/resources">
+      <el-icon><Files /></el-icon>资源</el-menu-item
+    >
+  </el-menu>
+  <div class="body_container">
+    <router-view />
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.body_contianer {
+  margin: 10px;
+}
+</style>
